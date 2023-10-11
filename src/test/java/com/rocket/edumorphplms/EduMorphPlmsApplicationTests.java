ffe6c56e3dbd1a0eee1c149d9b8d48cc9f4fc4b0
ez4bk <qq1034351717@gmail.com>
2023-10-11 17:01:19 -0400

    Spring initialized

---
+++ src/test/java/com/rocket/edumorphplms/EduMorphPlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.edumorphplms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduMorphPlmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
